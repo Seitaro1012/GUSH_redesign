7e7247f93d2a82914cb7e665d1f14993b5a07f2f
kitahara <mizupe1223@gmail.com>
2024-04-07 21:47:39 +0900

    ヘッダーフッターJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+import { gu_header, gu_footer } from './layout.js';
 // const openChild = () =>{
 //   const childs= document.querySelectorAll('.child_menu');
 //     childs.forEach( child => {
@@ -12,55 +12,18 @@
 
 
 
+document.addEventListener('DOMContentLoaded',function(){
 
-// $(function() {
-//   $.ajax({
-//     url: '../common/header.html', 
-//     dataType: 'html', 
-    
-//     success: function (data) {
-//       $('body').prepend(data);
-//     },
-//     error: function () {
-//       alert('header error!');
-//     },
-//   });
-//   $.ajax({
-//     url: '../common/footer.html', 
-//     dataType: 'html', 
-//     success: function (data) {
-//       $('footer').prepend(data);
-//     },
-//     error: function () {
-//       alert('footer error!');
-//     },
-//   });
-  
-// });
+  document.getElementById('gu-header-js').insertAdjacentHTML('afterbegin', gu_header);
+  document.getElementById('gu-footer-js').insertAdjacentHTML('afterbegin', gu_footer);
 
+  document.querySelector('.js-toggleButton').addEventListener('click', function(){
+    document.querySelector('body').classList.toggle('js-overlay');
+    const ol = document.querySelector('.gu-overlay');
+    ol.classList.toggle('js-show');
+    document.querySelector('.gu-mobile-menu-icon').classList.toggle('js-open');
 
-const overlay = () =>{
-  // let dts = document.querySelectorAll('dt');
-  document.querySelector('body').classList.toggle('js-overlay');
-  const ol = document.querySelector('.gu-overlay');
-  ol.classList.toggle('js-show');
-  document.querySelector('.gu-mobile-menu-icon').classList.toggle('js-open');
-  if ( !ol.classList.contains('js-show') ){
-    // dts.forEach(dt => {
-    //   dt.parentNode.classList.remove('appear');
-    //   let dts = 0;
-    // });
-  }
-  // if ( dts !== 0){
-  //   dts.forEach(dt => {
-  //     dt.addEventListener('click', ()=>{
-  //       dt.parentNode.classList.toggle('appear');
-  //       dts.forEach(el => {
-  //         if (dt !== el) {
-  //           el.parentNode.classList.remove('appear');
-  //         }
-  //       });
-  //     });
-  //   });
-  // }
-}
+  });
+
+});
+
